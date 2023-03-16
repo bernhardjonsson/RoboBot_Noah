44c30274ac8d2d98d1051080b85209e72ca22298
cnovrup <christian@novrup.com>
2023-03-16 09:28:24 +0100

    bug fix

--- main.cpp
+++ main.cpp
@@ -110,14 +110,14 @@ void golf(){
 		/*Upose startpose;
 		startpose.x = 0;
 		startpose.y = 0;
-		startpose.h = 0;
+		startpose.h = 0;*/
 		
 		Upose endpose;
 		endpose.x = vision.ball_x;
 		endpose.y = vision.ball_y;
 		endpose.h = 0;
-		PointToPoint::goToPoint(&startpose, &endpose, 0.1, 0.5);*/
-		string cmdHead = "regbot madd tr=0:turn=" + to_string(angle) + "\n";
+		PointToPoint::goToPoint(&pose, &endpose, 0.1, 0.5);*/
+		/*string cmdHead = "regbot madd tr=0:turn=" + to_string(angle) + "\n";
 		const char *head_char = cmdHead.c_str();
 				
 		string cmdDist = "regbot madd vel=0.1,acc=0.5:dist=" + to_string(dist) + "\n";
@@ -129,7 +129,7 @@ void golf(){
 		bridge.tx(head_char);
 		bridge.tx(dist_char);
 		bridge.tx("regbot start\n");
-		//event.waitForEvent(0);
+		//event.waitForEvent(0);*/
 	}
 	
 }
