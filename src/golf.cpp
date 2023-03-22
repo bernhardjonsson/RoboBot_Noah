316c90ee28394deae3742acba2c463e2f5997755
cnovrup <christian@novrup.com>
2023-03-22 13:52:55 +0100

    testing

--- src/golf.cpp
+++ src/golf.cpp
@@ -193,8 +193,8 @@ void minigolf_test(){
 	printf("\n");
 	
 	p2p.goToPoint(&localpose, &goto_pose, 0.1, 0.5);
-	curr_pose->x  = goto_pose.x + curr_pose->x;
-	curr_pose->y  = goto_pose.y + curr_pose->y;
+	curr_pose->x  = goto_pose.x - curr_pose->x;
+	curr_pose->y  = goto_pose.y - curr_pose->y;
 	curr_pose->h  = goto_pose.h;
 	
 	angle = atan2(0 - curr_pose->y, 0 - curr_pose->x);
@@ -204,8 +204,8 @@ void minigolf_test(){
 	
 	printf("want  to go to (origin):\n");
 	cout << "x : " + to_string(goto_pose.x) << endl;
-    cout << "y : " + to_string(goto_pose.x) << endl;
-    cout << "h : " + to_string(goto_pose.x) << endl;
+    cout << "y : " + to_string(goto_pose.y) << endl;
+    cout << "h : " + to_string(goto_pose.h) << endl;
 	printf("\n");
 	
 	p2p.goToPoint(&localpose, &goto_pose, 0.1, 0.5);
