b88a177dbafdb6c4cc297a53e87b5139d0a07534
cnovrup <christian@novrup.com>
2023-03-22 11:30:53 +0100

    testing

--- src/golf.cpp
+++ src/golf.cpp
@@ -159,11 +159,11 @@ void minigolf_test(){
 	UPose endpose;
 
 	angle = atan2(MINIGOLF_HOLE_Y - curr_pose->y, MINIGOLF_HOLE_X - curr_pose->x);
-	endpose.x =  MINIGOLF_HOLE_X - ARM_LENGTH*cos(angle)- curr_pose->x
-	endpose.y = MINIGOLF_HOLE_Y - ARM_LENGTH*sin(angle)- curr_pose->y
+	endpose.x =  MINIGOLF_HOLE_X - ARM_LENGTH*cos(angle)- curr_pose->x;
+	endpose.y = MINIGOLF_HOLE_Y - ARM_LENGTH*sin(angle)- curr_pose->y;
 	endpose.h = angle;
 	
-	p2p.goToPoint(curr_pose, &endpose 0.1, 0.5);
+	p2p.goToPoint(curr_pose, &endpose, 0.1, 0.5);
 	
 	
 	
