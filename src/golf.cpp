7b107970b75b85e5b735a7006a71d6defe42422a
cnovrup <christian@novrup.com>
2023-03-22 14:48:22 +0100

    testing

--- src/golf.cpp
+++ src/golf.cpp
@@ -204,8 +204,8 @@ void minigolf_test(){
 	printf("\n");
 	
 	angle = atan2(0 - curr_pose->y, 0 - curr_pose->x);
-	goto_pose.x =  0 - curr_pose->x;
-	goto_pose.y = 0 - curr_pose->y;
+	goto_pose.x =  0 + curr_pose->x;
+	goto_pose.y = 0 + curr_pose->y;
 	goto_pose.h = angle;
 	
 	
